da2c79349c8b64777e80cc20c1b172459fb1a276
FedkinAndrey <fedkin17@gmail.com>
2020-08-22 12:54:33 +0300

    Building the Likes Controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 import Search from "./models/Search"
 import Recipe from "./models/Recipe";
 import List from "./models/List";
+import Likes from "./models/Likes";
 import * as searchView from './views/searchView'
 import * as recipeView from './views/recipeView'
 import * as listView from './views/listView'
 import {elements, renderLoader, clearLoader} from "./views/base"
+
 
 /*Global state of the app
 * - search object
@@ -116,6 +118,41 @@ const controlList = () => {
     })
 }
 
+
+/*
+*LIKE CONTROLLER
+*/
+
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes()
+    const currentID = state.recipe.id
+
+    //user has not yet liked current recipe
+    if (!state.likes.isLiked(currentID)) {
+        //add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        )
+        //toggle the like button
+
+        //add like to UI list
+        console.log(state.likes)
+
+        //user has liked current recipe
+    } else {
+        //remove like from the state
+        state.likes.deleteLike(currentID)
+        //toggle the like button
+
+        //remove like from UI list
+        console.log(state.likes)
+    }
+}
+
+
 //handle delete and update list item events
 elements.shopping.addEventListener('click', e => {
     const id = e.target.closest('.shopping__item').dataset.itemid
@@ -149,7 +186,11 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc')
         recipeView.updateServingsIngredients(state.recipe)
     } else if (e.target.matches('recipe__btn--add, .recipe__btn--add *')) {
+        //add ingredients to shopping list
         controlList()
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        // like controller
+        controlLike()
     }
     // console.log(state.recipe)
 })
